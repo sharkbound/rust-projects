23c5dae5e50cebba0e7aeaa67fba819979aaccfa
sharkbound <laptopblaster@gmail.com>
2023-05-25 16:53:42 -0500

    note added

--- sql_database_example/src/main.rs
+++ sql_database_example/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     // had to use these to help install diesel and get it running with sqlite:
     // https://github.com/diesel-rs/diesel/issues/487
     // https://users.rust-lang.org/t/struggling-with-diesel-sqlite-on-windows/73336
+    // can also used "bundled" feature with libsqlite3-sys (but its not recommended)
     dotenv().ok();
 
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
